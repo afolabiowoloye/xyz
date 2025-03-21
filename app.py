9a41d2ec50b9a610810950178dced38539d2ad1f
afolabiowoloye <68096256+afolabiowoloye@users.noreply.github.com>
2025-03-21 21:10:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ selected = option_menu(
 if selected == "Home":
     st.markdown("""
     <h3 style='color: green;'>Welcome to Breast Cancer pIC50 Prediction Web App</h3>
-    st.write("We are thrilled to have you here. This app is designed to help researchers, clinicians, and scientists predict the **pIC50 values** of compounds targeting **20 different breast cancer targets**. Whether you're exploring potential drug candidates or analyzing molecular interactions, this tool is here to simplify your work and accelerate your discoveries.")
+    We are thrilled to have you here. This app is designed to help researchers, clinicians, and scientists predict the **pIC50 values** of compounds targeting **20 different breast cancer targets**. Whether you're exploring potential drug candidates or analyzing molecular interactions, this tool is here to simplify your work and accelerate your discoveries.")
     """, unsafe_allow_html=True)
     
 # Display data preview
