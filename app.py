f8643a873cd11fad69e420701f2ee08fac71901a
afolabiowoloye <68096256+afolabiowoloye@users.noreply.github.com>
2025-03-21 22:15:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -77,11 +77,12 @@ if selected == "Home":
     with st.sidebar.header("""Overview and Usage"""):
         st.sidebar.markdown("""
         <h4 style='color: blue;'>Brief Overview of the App</h4>
-        The <strong>Breast Cancer pIC<sub>50</sub> Predictor</strong> is a powerful tool that leverages advanced <em>machine learning algorithms<?em> to predict the <strong>pIC<sub>50</sub> values</strong> of compounds. The pIC50 value is a critical metric in drug discovery, representing the potency of a compound in inhibiting a specific target.<br>
+        The <strong>Breast Cancer pIC<sub>50</sub> Predictor</strong> is a powerful tool that leverages advanced <em>machine learning algorithms</em> to predict the <strong>pIC<sub>50</sub> values</strong> of compounds. The pIC50 value is a critical metric in drug discovery, representing the potency of a compound in inhibiting a specific target.<br>
                
         <h4 style='color: blue;'>How to Use the App</h4>
         <strong>1. Select a Target:</strong> Choose one of the 20 breast cancer targets from Home page.<br>
-        <strong>2. Input Your Compound:</strong> Upload compounds' SMILES string file. See [Example input file](https://raw.githubusercontent.com/afolabiowoloye/xyz/refs/heads/main/sample.csv).<br>
+        <strong>2. Input Your Compound:</strong> Upload compounds' SMILES string file.<br>
+        [Example input file](https://raw.githubusercontent.com/afolabiowoloye/xyz/refs/heads/main/sample.csv).<br>
         <strong>3. Get Predictions:</strong> Click <strong>Predict</strong> to receive the <sub>50</sub> value for your compound.<br>
         <strong>4. Explore Results:</strong> View detailed predictions and download the results for further analysis.<br>
               
