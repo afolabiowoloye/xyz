b33799346cfe7765f008eeb33017f399fd2c4f02
afolabiowoloye <68096256+afolabiowoloye@users.noreply.github.com>
2025-04-11 15:24:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ targets = ['ER', 'Aromatase', 'CDK2', 'Braf', 'PI3K', 'VEGFR2', 'mTOR', 'PARP1',
 if selected == "Home":
     st.markdown("""
     <h3 style='color: darkblue;'>Welcome to Breast Cancer pIC<sub>50</sub> Prediction Web App</h3>
-    We are thrilled to have you here. This app is designed to help researchers, clinicians, and scientists predict the <strong>pIC<sub>50</sub> values</strong> of compounds targeting <strong>20 different breast cancer targets</strong>. Whether you're exploring potential drug candidates or analyzing molecular interactions, this tool is here to simplify your work and accelerate your discoveries.
+    We are thrilled to have you here. This app is designed to help researchers, clinicians, and scientists predict the <strong>pIC<sub>50</sub> values</strong> of compounds targeting <strong>20 different breast cancer targets</strong>. Whether exploring potential drug candidates or analyzing molecular interactions, this tool is here to simplify your work and accelerate your discoveries.
     """, unsafe_allow_html=True)
     
     st.markdown("""
@@ -87,10 +87,10 @@ if selected == "Home":
     with st.sidebar.header("""Overview and Usage"""):
         st.sidebar.markdown("""
         <h4 style='color: blue;'>Brief Overview of the App</h4>
-        The <strong>Breast Cancer pIC<sub>50</sub> Predictor</strong> is a powerful tool that leverages advanced <em>machine learning algorithms</em> to predict the <strong>pIC<sub>50</sub> values</strong> of compounds. The pIC<sub>50</sub> value is a critical metric in drug discovery, representing the potency of a compound in inhibiting a specific target.<br>
+        The <strong>Breast Cancer pIC<sub>50</sub> Predictor</strong> is a powerful tool that leverages on advanced <em>machine learning algorithms</em> to predict the <strong>pIC<sub>50</sub> values</strong> of compounds. The pIC<sub>50</sub> value is a critical metric in drug discovery, representing the potency of a compound in inhibiting a specific target.<br>
                
         <h4 style='color: blue;'>How to Use the App</h4>
-        <strong>1. Select a Target:</strong> Choose one of the 20 breast cancer targets from Home page.<br>
+        <strong>1. Select a Target:</strong> Choose one of the 20 breast cancer targets from the Home page.<br>
         <strong>2. Input Your Compound:</strong> Upload compounds' SMILES string file.<br>
         <strong>3. Get Predictions:</strong> Click <strong>Predict</strong> to receive the <sub>50</sub> value for your compound.<br>
         <strong>4. Explore Results:</strong> View detailed predictions and download the results for further analysis.<br>
@@ -101,7 +101,7 @@ if selected == "Home":
         <strong>Accelerate Research:</strong> Streamline your drug discovery workflow and focus on the most promising leads.<br>
             
         <h4 style='color: blue;'>Get Started</h4>
-        Ready to explore? Click your <strong>target of choice</strong> button to begin your journey toward discovering potent breast cancer inhibitors. If you have any questions or need assistance, please contact us.
+        Ready to explore? Click on the <strong>Select Target</strong> button to begin your journey toward discovering potent breast cancer inhibitors. If you have any questions or need assistance, please contact us.
         """, unsafe_allow_html=True)
         st.markdown("""[Example input file](https://raw.githubusercontent.com/afolabiowoloye/xyz/refs/heads/main/sample.csv)""")
 
